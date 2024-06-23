5337bb63b60d0631be5d47da72486f52b730027b
unknown <tom.eckstein@web.de>
2024-06-23 18:17:58 +0200

    Add battle mode page for images

--- backend/api/app.py
+++ backend/api/app.py
@@ -7,11 +7,16 @@ import json
 app = Flask(__name__)
 
 VOTES_LOCATION = 'votes.json'
+BATTLES_LOCATION = 'battles.json'
 votes = defaultdict(lambda: {'likes': 0, 'dislikes': 0})
+battles = defaultdict(lambda: {'wins': 0, 'losses': 0})
 
 if os.path.exists(VOTES_LOCATION):
   with open(VOTES_LOCATION, 'r') as file:
     votes = json.load(file)
+if os.path.exists(BATTLES_LOCATION):
+  with open(BATTLES_LOCATION, 'r') as file:
+    battles = json.load(file)
 
 # List of image URLs
 images = os.listdir(os.path.join(os.path.dirname(__file__), 'static/images'))
@@ -22,6 +27,16 @@ images = os.listdir(os.path.join(os.path.dirname(__file__), 'static/images'))
 def get_home_page():
   '''Returns the home page of the app'''
   return render_template('index.html')
+
+@app.route('/battle', methods=['GET'])
+def get_battle_page():
+  '''Returns the battle page of the app'''
+  return render_template('battle.html')
+
+@app.route('/battle/data', methods=['GET'])
+def get_battle_data():
+  '''Returns the battle results of the app'''
+  return jsonify(battles)
 
 @app.route('/refresh', methods=['GET'])
 def get_refresh_page():
@@ -88,6 +103,26 @@ def refresh():
   '''Refreshes the images internally'''
   global images
   images = os.listdir(os.path.join(os.path.dirname(__file__), 'static/images'))
+  return '', 204
+
+
+@app.route('/api/battle/vote', methods=['POST'])
+def vote_battle():
+  '''
+  Expects a form parameter 'imageUrl' which is the full url of the image to vote on
+  Increments the number of dislikes for the image at the given index
+  '''
+  winner_image_url = request.form.get('winnerUrl')
+  looser_image_url = request.form.get('looserUrl')
+  if winner_image_url is None or looser_image_url is None:
+    return 'No image URL provided', 400
+  win_image = winner_image_url.split('/')[-1]
+  battles[win_image]['wins'] += 1
+  loss_image = looser_image_url.split('/')[-1]
+  battles[loss_image]['losses'] += 1
+  # Save battles to a file
+  with open(BATTLES_LOCATION, 'w') as file:
+    json.dump(battles, file)
   return '', 204
 
 #endregion
